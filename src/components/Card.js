5e00aeaa7596454fb36229eeb6eaca0269ca9d06
Mia <MiaRoseElbo@gmail.com>
2024-06-22 16:36:03 -0400

    Add icons to habilidad box

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,23 +1,32 @@
-// src/components/Card.js
 import React from 'react';
 import './Card.css';
-import { PersonajeIcon } from './Icons';
+import Icons from './Icons';
 import tipoMapping from '../utils/cardJsonMapping'; // Import the mapping object
 import getImagePath from '../utils/getImagePath';
 
 const iconMapping = {
-  '[Personaje]': <PersonajeIcon />,
-//   '[Empatía]': <EmpatiaIcon />,
+  'Personaje': (<Icons type="per" />),
+  'Santuario': (<Icons type="san" />),
+  'Tecnología': (<Icons type="tec" />),
+  'Manipulación': (<Icons type="man" />),
+  'Coloso': (<Icons type="col" />),
+  'Aditamento': (<Icons type="adi" />),
 };
 
 const Card = ({ card }) => {
   const { Habilidad } = card;
 
   const renderHabilidad = (text) => {
-    const parts = text.split(/(\[.*?\])/);
+    const regex = new RegExp(`(${Object.keys(iconMapping).join('|')})`, 'g');
+    const parts = text.split(regex);
+    
     return parts.map((part, index) => {
       if (iconMapping[part]) {
-        return <React.Fragment key={index}>{iconMapping[part]}</React.Fragment>;
+        return (
+          <React.Fragment key={index}>
+            {iconMapping[part]} {part}
+          </React.Fragment>
+        );
       }
       return <React.Fragment key={index}>{part}</React.Fragment>;
     });
@@ -44,29 +53,30 @@ const Card = ({ card }) => {
     ));
   };
 
-  const cardImagePath = card.Imagen?getImagePath('cards', `${card.Imagen}.jpg`):getImagePath('cards', `${card.Edicion}${card.Numero}.jpg`);
+  const cardImagePath = card.Imagen
+    ? getImagePath('cards', `${card.Imagen}.jpg`)
+    : getImagePath('cards', `${card.Edicion}${card.Numero}.jpg`);
   const cardFramePath = getImagePath('frames', `${card.Marco}.png`);
-
 
   return (
     <div className="card-container">
-      <div className="card-image solid-frame" style={{ backgroundImage: `url(${cardImagePath})` }}>
+      <div className="card-image" style={{ backgroundImage: `url(${cardImagePath})` }}>
         <div className="card-frame" style={{ backgroundImage: `url(${cardFramePath})` }}>
           <div className="card-name">
             <h1 className="name">{card.Nombre}</h1>
           </div>
           {renderSpheres()}
           <div className="card-icon">
-            <PersonajeIcon />
+            <Icons type={card.Tipo} fill="white" />
           </div>
           <div className="card-type">
-            <h1 className="type">{tipoMapping[card.Tipo] || card.Tipo}</h1>
+            <h1 className="type">{(tipoMapping[card.Tipo] || card.Tipo) + (card.Subtipo ? " - " + card.Subtipo : "")}</h1>
           </div>
           <div className="card-text">
             <p className="habilidad">{renderHabilidad(Habilidad)}</p>
             <p className="epigrafe">{card.Epigrafe}</p>
             <div className="card-illus">{`Ilustrador: ${card.Ilustrador}`}</div>
-            <div className="card-rarity">{(card.Frecuencia?card.Frecuencia:'')+' '+card.Edicion+card.Numero}</div>
+            <div className="card-rarity">{(card.Frecuencia ? card.Frecuencia : '') + ' ' + card.Edicion + card.Numero}</div>
           </div>
         </div>
       </div>

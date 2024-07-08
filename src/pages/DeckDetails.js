bf622dd3e776bf53b8ecf21fd737fc2e3477af2d
Mia <MiaRoseElbo@gmail.com>
2024-07-08 13:42:33 -0400

    Add plots to deck detail

--- src/pages/DeckDetails.js
+++ src/pages/DeckDetails.js
@@ -4,6 +4,8 @@ import { database } from '../firebase';
 import { ref, get } from 'firebase/database';
 import cardsData from '../data/cards.json';
 import Card from '../components/Card';
+import { Bar, Line } from 'react-chartjs-2';
+import 'chart.js/auto';
 import './DeckDetails.css';
 
 const DeckDetails = () => {
@@ -93,12 +95,77 @@ const DeckDetails = () => {
     );
   };
 
+  const getCardTypeData = (groupedCards) => {
+    const typesOrder = ['col', 'per', 'man', 'tec', 'adi'];
+    const data = typesOrder.map((type) => groupedCards[type].length);
+    return data;
+  };
+
+  const getCardCostData = (cards) => {
+    let minCost = Infinity;
+    let maxCost = -Infinity;
+
+    cards.forEach((id) => {
+      const card = cardsData.find((card) => card.id === id);
+      if (card) {
+        const cost = card.Coste || 0;
+        if (cost < minCost) minCost = cost;
+        if (cost > maxCost) maxCost = cost;
+      }
+    });
+
+    const costCounts = new Array(maxCost - minCost + 1).fill(0);
+
+    cards.forEach((id) => {
+      const card = cardsData.find((card) => card.id === id);
+      if (card) {
+        const cost = card.Coste || 0;
+        costCounts[cost - minCost] += 1;
+      }
+    });
+
+    return {
+      minCost,
+      maxCost,
+      costCounts,
+    };
+  };
+
   if (!deck) {
     return <div>Loading...</div>;
   }
 
   const groupedCards = groupCards(deck.cards);
   const tabs = ['col', 'per', 'man', 'tec', 'adi'];
+
+  const cardTypeData = {
+    labels: ['COL', 'PER', 'MAN', 'TEC', 'ADI'],
+    datasets: [
+      {
+        label: 'Tipo de Cartas',
+        data: getCardTypeData(groupedCards),
+        backgroundColor: 'rgba(75, 192, 192, 0.6)',
+        borderColor: 'rgba(75, 192, 192, 1)',
+        borderWidth: 1,
+      },
+    ],
+  };
+
+  const { minCost, maxCost, costCounts } = getCardCostData(deck.cards);
+  const costLabels = Array.from({ length: maxCost - minCost + 1 }, (_, i) => minCost + i);
+
+  const cardCostData = {
+    labels: costLabels,
+    datasets: [
+      {
+        label: 'Cartas por coste',
+        data: costCounts,
+        backgroundColor: 'rgba(153, 102, 255, 0.6)',
+        borderColor: 'rgba(153, 102, 255, 1)',
+        borderWidth: 1,
+      },
+    ],
+  };
 
   return (
     <div className="deck-details">
@@ -108,6 +175,14 @@ const DeckDetails = () => {
       </div>
       <div className="san-cards">
         {renderCardGroup(groupedCards['san'])}
+        <div className="deck-detail-charts">
+          <div className="deck-detail-chart">
+            <Bar data={cardTypeData} options={{ responsive: true }} />
+          </div>
+          <div className="deck-detail-chart">
+            <Bar data={cardCostData} options={{ responsive: true }} />
+          </div>
+        </div>
       </div>
       <div className="deck-cards">
         <div className="tabs">
